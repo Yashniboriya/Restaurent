8642968f759a84244dc89e7b95d6e4111af3a75c
Sachinjha08 <sachinjha08@gmail.com>
2024-03-07 00:10:40 +0530

    frontend

---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">Tasty Resturent</div>
+          <div className="right">
+            <p>Shantinagar Mhow Indore,MP</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By preditor</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By preditor.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
