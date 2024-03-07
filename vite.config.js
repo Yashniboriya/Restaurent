f308b41def8503071ea8ec26efb0cd4c27c63112
Sachinjha08 <sachinjha08@gmail.com>
2024-03-07 11:53:11 +0530

    resturent

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // server: {
-  //   proxy: {
-  //     "/api": {
-  //       target: "http://localhost:5000", // Update this with the correct backend server address
-  //       changeOrigin: true,
-  //       rewrite: (path) => path.replace(/^\/api/, ""),
-  //     },
-  //   },
-  // },
   plugins: [react()],
 });
